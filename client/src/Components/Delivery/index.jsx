17b46cebb3b908f843517f6aa7b8bd470a69744f
arpita1329 <arpitadubey1329@gmail.com>
2021-08-23 11:58:30 +0530

    Restaurant Card Added

--- client/src/Components/Delivery/index.jsx
+++ client/src/Components/Delivery/index.jsx
@@ -3,11 +3,13 @@ import React from "react";
 // Components
 import DeliveryCarousal from "./DeliveryCarousal";
 import Brand from "./Brand";
+import RestaurantCard from "../restaurantCard";
 
 const Delivery = () => {
     return (
         <>
             <DeliveryCarousal />
+            <RestaurantCard />
         </>
     )
 }
