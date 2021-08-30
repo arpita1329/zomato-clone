bd89a7cd6a6797177297b5275f69b0d478624764
arpita1329 <arpitadubey1329@gmail.com>
2021-08-30 22:28:58 +0530

    fix redirect

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,22 @@
+import {Route, Redirect} from "react-router-dom";
+
 // HOC
 import HomeLayoutHOC from "./HOC/Home.HOC";
 
 // Components
 import Temp from "./Components/temp";
-import Master from "./Components/master";
+
+// Page
+import Home from "./Page/Home";
 
 function App() {
   return (
     <>
+      <Route path="/" exact>
+        <Redirect to="/delivery" />
+      </Route>
       <HomeLayoutHOC path ="/" exact component={Temp} />
-      <HomeLayoutHOC path ="/:type" exact component={Master} />
+      <HomeLayoutHOC path ="/:type" exact component={Home} />
     </>
   );
 }
