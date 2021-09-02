c19d353f76aad43a1a835252d709f3d475d5d15f
arpita1329 <arpitadubey1329@gmail.com>
2021-09-02 18:29:37 +0530

    add floating button

---
+++ client/src/Page/Restaurant/OrderOnline.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+// Components
+import FloatMenuBtn from "../../Components/restaurant/Order-Online/FloatMenuBtn";
+
+const OrderOnline = () => {
+    return (
+        <>
+            <div className="w-full">
+                <FloatMenuBtn />
+            </div>
+        </>
+    );
+};
+
+export default OrderOnline;
