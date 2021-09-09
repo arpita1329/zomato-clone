dffcbee80db78f5075ccb4583b55829b5a15e184
arpita1329 <arpitadubey1329@gmail.com>
2021-09-09 22:57:56 +0530

    add menu and photos

--- client/src/Page/Restaurant/Photos.jsx
+++ client/src/Page/Restaurant/Photos.jsx
@@ -1,19 +1,33 @@
-import React, {useState} from "react";
+import React, { useState, useEffect } from "react";
 import ImageViewer from 'react-simple-image-viewer';
+import { useSelector, useDispatch } from "react-redux";
 
 // Components
 import PhotoCollection from "../../Components/restaurant/PhotosCollection";
 
+// Redux Actions
+import { getImage } from "../../Redux/Reducer/Image/image.action";
+
 const Photos = () => {
-    const [photos, setPhotos] = useState([
-        "https://b.zmtcdn.com/data/pictures/chains/8/100148/dea1f8e1bc5b7fe258483f4641b20a47.jpg",
-        "https://b.zmtcdn.com/data/pictures/chains/8/100148/a2a2cc63da72ef44a515d4b3f409a174.jpg",
-        "https://b.zmtcdn.com/data/pictures/chains/8/100148/bfdc584cc727cd8486988a0690fdad67.jpg",
-        "https://b.zmtcdn.com/data/pictures/chains/8/100148/22f627ba771b601acf2da434822fdffa.jpg",
-        "https://b.zmtcdn.com/data/pictures/chains/8/100148/09fbd766e559b279361dfd0ec7ba0140.jpg"
-    ]);
+    const [photos, setPhotos] = useState([]);
     const [ isMenuOpen, setIsMenuOpen] = useState(false);
     const [ CurrentImg, setCurrentImg] = useState(0);
+
+    const reduxState = useSelector(
+        (globalStore) => globalStore.restaurant.selectedRestaurant.restaurant
+      );
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        if (reduxState) {
+          dispatch(getImage(reduxState?.photos)).then((data) => {
+            const images = [];
+            data.payload.image.images.map(({ location }) => images.push(location));
+            setPhotos(images);
+          });
+        }
+      }, []);
+
     const closeViewer = () => setIsMenuOpen(false);
     const openViewer = () => setIsMenuOpen(true);
 
